45e67465d7f60a2700e97613c3d160587c8deab9
CRYPTO HODL <101343105+exfeddix17@users.noreply.github.com>
2022-04-04 16:46:15 +0300

    Update gear.sh

--- gear.sh
+++ gear.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo apt-get update -y
-sudo apt-get intall wget curl jq tree -y
+sudo apt update -y
+sudo apt install curl -y
+sudo apt intall wget jq tree -y
 
 curl -s https://raw.githubusercontent.com/exfeddix17/cryptohodl/main/cryptohodl.sh | bash && sleep 2
 
@@ -11,9 +12,3 @@ rm gear-nightly-linux-x86_64.tar.xz && \
 chmod +x gear-node
 sudo apt install -y clang build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup toolchain add nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-git clone https://github.com/gear-tech/gear.git
-cd gear
-cargo build -p gear-node --release
-
